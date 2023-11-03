bf99dcde4ad9c2c8a3a8588c993b3bdfcc667ac7
Louis Klein <louisklein71@gmail.com>
2023-11-03 14:31:16 -0700

    init

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,6 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import Links from './components/Links';
-import Projects from './components/Projects';
-import Summary from './components/Summary';
-import Resume from './components/Resume';
 
 // TODO sessionid create/check on open
 
@@ -18,12 +14,8 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      Unga Bunga
       <Routes>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/about" element={<Summary/>}/>
-        <Route path="/contact" element={<Links/>}/>
-        <Route path="/resume" element={<Resume/>}/>
+
         <Route path="/" element={<Home/>}/>
       </Routes>
       <Footer/>
