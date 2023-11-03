6e35feb4a350ec2a62ef217172d809e7eb9e2e3d
Louis Klein <louisklein71@gmail.com>
2023-11-03 14:34:52 -0700

    navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Smoke Plus </div>
-  )
+    <header className="header">
+      <div className="left-nav">
+        <Link to="/left">Left</Link>
+      </div>
+      <div className="logo">
+        <Link to="/">SMOKE PLUS</Link>
+      </div>
+      <div className="right-nav">
+        <Link to="/right">Right</Link>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
