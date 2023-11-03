62c913cb172854e16c933b329e38b18e0db1f8c1
Louis Klein <louisklein71@gmail.com>
2023-11-02 23:37:48 -0700

    employee components

---
+++ client/src/components/employees/EditInventory.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// create full list of products, click on to go into product edit
+const EditInventory = () => {
+  return (
+    <div>EditInventory</div>
+  )
+}
+
+export default EditInventory
---
+++ client/src/components/employees/ExcelUploader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExcelUploader = () => {
+  return (
+    <div>ExcelUploader</div>
+  )
+}
+
+export default ExcelUploader
