9d41518c5003c89ed52bd0d8a6367b15f3655596
Louis Klein <louisklein71@gmail.com>
2023-11-08 14:02:31 -0800

    banner test 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 // imports
 import './App.css';
-import React, {createContext, useState, useEffect} from 'react';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import React, { createContext, useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// banner images
+import hero1 from './img/hero1.png';
+import hero2 from './img/hero2.png';
+import hero3 from './img/hero3.png';
+import hero4 from './img/hero4.png';
 
 // components
 import Header from './components/Header';
@@ -14,21 +20,27 @@ import AllProducts from './components/AllProducts';
 import Banner from './components/Banner';
 
 // TODO sessionid create/check on open
-
-function App() {
+const App = () => {
+  const images = [
+    hero1,
+    hero2,
+    hero3,
+    hero4
+    // Add more image URLs as needed
+  ];
+  
   return (
     <div className="App">
-      <Header/>
-      <Banner/>
+      <Header />
+      <Banner images={images} />
       <Routes>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<Order/>}/>
-        <Route path='/all' element={<AllProducts/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-
-        <Route path="/" element={<Home/>}/>
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/all' element={<AllProducts />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path="/" element={<Home />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Banner = () => {
+const Banner = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  // Function to switch to the next image
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  // Automatically switch to the next image every few seconds
+  useEffect(() => {
+    const timer = setInterval(nextImage, 5000); // Change image every 5 seconds
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="banner">
+      <img src={images[currentImageIndex]} alt="Promotion" />
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
