e649b982e0351e8459de27303cdd132f1291d516
Louis Klein <louisklein71@gmail.com>
2023-11-09 15:59:13 -0800

    the banner is not garbage now

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 const Banner = ({ images }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  // Function to switch to the next image
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  // Automatically switch to the next image every few seconds
-  useEffect(() => {
-    const timer = setInterval(nextImage, 5000); // Change image every 5 seconds
-    return () => clearInterval(timer);
-  }, []);
-
-  return (
-    <div className="banner">
-      <img src={images[currentImageIndex]} alt="Promotion" />
-    </div>
-  );
-};
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  
+    const nextImage = () => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+  
+    const prevImage = () => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+    };
+  
+    useEffect(() => {
+      const timer = setInterval(nextImage, 3000); // Change image every 3 seconds
+      return () => clearInterval(timer);
+    }, [currentImageIndex]);
+  
+    return (
+        <div className="carousel">
+          <button className="prev" onClick={prevImage}>&lt;</button>
+          <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex}`} />
+          <button className="next" onClick={nextImage}>&gt;</button>
+        </div>
+      );
+    };
 
 export default Banner;
