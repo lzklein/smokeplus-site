bbbef9c5153c88ad6bbaa0eff45f3b1e60c0ccdc
Louis Klein <louisklein71@gmail.com>
2023-11-10 13:52:07 -0800

    Update EmployeeLogin.js

--- client/src/components/employees/EmployeeLogin.js
+++ client/src/components/employees/EmployeeLogin.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const EmployeeLogin = () => {
-  return (
-    <div>EmployeeLogin</div>
-  )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-export default EmployeeLogin
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: Handle the login logic here (e.g., send a request to the server)
+
+    // Clear the form after submission
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default EmployeeLogin;
