43e7527ea7f815b8c614419026d62ecefe156e13
Louis Klein <louisklein71@gmail.com>
2023-11-10 16:03:44 -0800

    sign in form

--- client/src/components/employees/EmployeeLogin.js
+++ client/src/components/employees/EmployeeLogin.js
@@ -3,41 +3,61 @@ import React, { useState } from 'react';
 const EmployeeLogin = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // TODO: Handle the login logic here (e.g., send a request to the server)
-
-    // Clear the form after submission
+    console.log("submitted");
     setUsername('');
     setPassword('');
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
+      <form onSubmit={handleSubmit} className="login">
+        <div>
+          <label htmlFor="username">Username</label>
+          <br />
+
           <input
             type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
+            name="username"
+            id="username"
+            className="rounded-md border py-1.5 pl-7 pr-20"
+            placeholder="username"
           />
-        </label>
+        </div>
         <br />
-        <label>
-          Password:
+        <div>
+          <label htmlFor="password">Password</label>
+          <br />
+
           <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
+            type={showPassword ? "text" : "password"}
+            name="password"
+            id="password"
+            className="rounded-md border py-1.5 pl-7 pr-20"
+            placeholder="password"
           />
-        </label>
+                  <br />
+
+          <label>
+            <input
+              type="checkbox"
+              onChange={() => setShowPassword(!showPassword)}
+            />{" "}
+            Show Password
+          </label>
+        </div>
         <br />
-        <button type="submit">Login</button>
+        <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 !"
+        >
+          Sign in
+        </button>
+        </div>
       </form>
     </div>
   );
