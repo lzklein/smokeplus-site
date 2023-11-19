1d4142df11a8c51e11b39cd01ae3f77dfb8ca7a0
Louis <louisklein71@gmail.com>
2023-11-19 01:15:53 -0800

    new setup

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,10 +6,12 @@ loginRouter.post('/', (req, res) => {
   console.log("Login route triggered");
   
   const { username, password } = req.body;
-  const { USERNAME, PASSWORD } = process.env;
+  const {LOGIN, PASSWORD} = process.env;
 
+  console.log(username, password);
+  console.log(LOGIN, PASSWORD);
   // Compare with the environment variables
-  if (username === USERNAME && password === PASSWORD) {
+  if (username === LOGIN && password === PASSWORD) {
     res.json({ message: 'Login successful', user: { username } });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
