41e267d4e35a22f4aecbbf79d46c15362be9fec9
Louis <louisklein71@gmail.com>
2023-11-23 17:50:19 -0800

    product card prep

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const ProductCard = () => {
+
+  const handleNavigate = () => {
+    // navigate to /product/product.name or product.id or something
+  }
+
   return (
-    <div>ProductCard</div>
+    <div className="productcard" onClick={handleNavigate()}>
+      <img/>
+      <h4>Product name</h4>
+      <p>Price</p>
+    </div>
   )
 }
 
