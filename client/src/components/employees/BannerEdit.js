fcb990fb3e97e516badba1dc9e4c257612c097de
Louis <louisklein71@gmail.com>
2023-11-24 18:52:43 -0800

    banner edit v1 (no persist)

--- client/src/components/employees/BannerEdit.js
+++ client/src/components/employees/BannerEdit.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const BannerEdit = ({ bannerImages }) => {
   const navigate = useNavigate();
+  const [updatedBannerImages, setUpdatedBannerImages] = useState(bannerImages);
+
+  useEffect(() => {
+    localStorage.setItem('bannerImages', JSON.stringify(updatedBannerImages));
+  }, [updatedBannerImages]);
+
+  const handleDelete = (index) => {
+    const isConfirmed = window.confirm('Are you sure you want to delete this image?');
+    
+    if (isConfirmed) {
+      const newImages = [...updatedBannerImages];
+      newImages.splice(index, 1);
+      setUpdatedBannerImages(newImages);
+    }
+  };
+
+  const handleAddNewImage = () => {
+    const newImageURL = prompt('Enter the URL for the new image:');
+    if (newImageURL) {
+      setUpdatedBannerImages([...updatedBannerImages, newImageURL]);
+    }
+  };
 
   return (
     <div>
@@ -11,12 +33,17 @@ const BannerEdit = ({ bannerImages }) => {
         {"<< Back"}
       </button>
       <div className="banner-cards">
-        {bannerImages.map((image, index) => (
+        {updatedBannerImages.map((image, index) => (
           <div key={index} className="banner-card">
             <img src={image} alt={`Banner ${index + 1}`} />
-            {/* Add other content for the banner card as needed */}
+            <button className="backbutton" onClick={() => handleDelete(index)}>
+              Delete
+            </button>
           </div>
         ))}
+        <button className="backbutton" onClick={handleAddNewImage}>
+          + Add New Image
+        </button>
       </div>
     </div>
   );
