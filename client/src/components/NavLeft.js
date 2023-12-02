56747858b92cb257d7b9c0a6eb24d07fe7d7a868
Louis Klein <louisklein71@gmail.com>
2023-12-01 21:48:20 -0800

    css

--- client/src/components/NavLeft.js
+++ client/src/components/NavLeft.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const NavLeft = () => {
   return (
     <div>
-        <Link to="/all">All Products</Link>
-        <Link to="/contact">Contact Us</Link>
+        <Link to="/all" >All Products</Link>
+        <Link to="/contact" style={{"margin-left":"80px", "margin-right":"80px"}}>Contact Us</Link>
     </div>
   )
 }
