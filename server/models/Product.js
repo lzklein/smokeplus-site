906892c720c0d2138ba80e52e7cb37c1e19cd6e4
Louis Klein <louisklein71@gmail.com>
2023-12-10 03:16:08 -0800

    size & flavor added

--- server/models/Product.js
+++ server/models/Product.js
@@ -9,7 +9,6 @@ const Product = sequelize.define('Product', {
   categories: {
     type: DataTypes.STRING,
     allowNull: false,
-    // defaultValue: '', // Set a default value if needed
     get() {
       return this.getDataValue('categories').split(',');
     },
@@ -22,8 +21,39 @@ const Product = sequelize.define('Product', {
         console.warn('Invalid value type for categories:', typeof value);
       }
     },
-    
-  },  
+  },
+  sizes: {
+    type: DataTypes.STRING, 
+    allowNull: true,
+    get() {
+      return this.getDataValue('sizes').split(',');
+    },
+    set(value) {
+      if (Array.isArray(value)) {
+        this.setDataValue('sizes', value.join(','));
+      } else if (typeof value === 'string') {
+        this.setDataValue('sizes', value);
+      } else {
+        console.warn('Invalid value type for sizes:', typeof value);
+      }
+    },
+  },
+  flavors: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    get() {
+      return this.getDataValue('flavors').split(',');
+    },
+    set(value) {
+      if (Array.isArray(value)) {
+        this.setDataValue('flavors', value.join(','));
+      } else if (typeof value === 'string') {
+        this.setDataValue('flavors', value);
+      } else {
+        console.warn('Invalid value type for flavors:', typeof value);
+      }
+    },
+  },
   price: {
     type: DataTypes.FLOAT,
     allowNull: false,
@@ -35,21 +65,21 @@ const Product = sequelize.define('Product', {
     type: DataTypes.INTEGER,
     allowNull: false,
     validate: {
-      min: 0, 
+      min: 0,
     },
   },
   description: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true, 
+      notEmpty: true,
     },
   },
   image: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true, 
+      notEmpty: true,
     },
   },
   id: {
