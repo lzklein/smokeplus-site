a01879c1a002b0e4e9afde4904fb7109833bfb08
Louis Klein <louisklein71@gmail.com>
2023-12-11 04:05:39 -0800

    db update

--- server/migrations/20231211120330-add_product_model.js
+++ server/migrations/20231211120330-add_product_model.js
@@ -1,72 +1,73 @@
+'use strict';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Products', {
       id: {
+        type: Sequelize.INTEGER,
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       categories: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       sizes: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
       flavors: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: true,
       },
       price: {
         type: Sequelize.FLOAT,
         allowNull: false,
         validate: {
-          min: 0
-        }
+          min: 0,
+        },
       },
       quantity: {
         type: Sequelize.INTEGER,
         allowNull: false,
         validate: {
-          min: 0
-        }
+          min: 0,
+        },
       },
       description: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
-          notEmpty: true
-        }
+          notEmpty: true,
+        },
       },
       image: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
-          notEmpty: true
-        }
+          notEmpty: true,
+        },
       },
       deals: {
         type: Sequelize.INTEGER,
-        defaultValue: 0
+        defaultValue: 0,
       },
       createdAt: {
+        type: Sequelize.DATE,
         allowNull: false,
-        type: Sequelize.DATE
       },
       updatedAt: {
+        type: Sequelize.DATE,
         allowNull: false,
-        type: Sequelize.DATE
-      }
+      },
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Products');
-  }
+  },
 };
