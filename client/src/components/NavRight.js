48eb1cee5ca133072c98c5c6911da12ba3f158bf
Louis Klein <louisklein71@gmail.com>
2023-12-18 04:52:14 -0800

    hyphen gone

--- client/src/components/NavRight.js
+++ client/src/components/NavRight.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NavRight = ({cart}) => {
   return (
     <div>
-        <Link to="/order" className="headnav" style={{"margin-left":"80px", "margin-right":"80px"}}>Order Status</Link>
+        <Link to="/order" className="headnav" style={{marginLeft:"80px", marginRight:"80px"}}>Order Status</Link>
         <Link to="/cart" className="headnav">Cart ({cart.length})</Link>
     </div>
   )
