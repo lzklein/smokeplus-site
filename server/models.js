c2dc6fec66c0386d8a53400699ef4a7436fad3a7
Louis Klein <louisklein71@gmail.com>
2023-12-18 01:03:16 -0800

    Cart model created

--- server/models.js
+++ server/models.js
@@ -2,9 +2,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = require('./config');
 
 // imports
-
 const Product = require('./models/Product');
+const Cart = require('./models/Cart');
+
+// relationships
+Cart.belongsTo(Product, { foreignKey: 'product' });
 
 
 // Export
-module.exports = { sequelize, Product };
+module.exports = { sequelize, Product, Cart };
--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,4 +1,33 @@
-// item.id and user.id join for items in cart
+// use product.id from Products in cart
+// basic id, user(sessionId), 
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config');
+const Product = require('./Product');
 
-// product.id
-// user.id
+const Cart = sequelize.define('Cart', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        },
+    user: {
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    },
+    product: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        references: {
+            model: Product,
+            key: 'id',
+        },
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull:false
+    }
+});
+
+module.exports = Cart;
