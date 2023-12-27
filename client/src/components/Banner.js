d0c040076432a5dbd0972809fc4df49c8f14b54f
Louis Klein <louisklein71@gmail.com>
2023-12-26 20:21:08 -0800

    Update Banner.js

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from 'react';
-// import '../styles/Banner.css';
 
 const Banner = ({ images }) => {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  
-    const nextImage = () => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-  
-    const prevImage = () => {
-      setCurrentImageIndex((prevIndex) =>
-        prevIndex === 0 ? images.length - 1 : prevIndex - 1
-      );
-    };
-  
-    useEffect(() => {
-      const timer = setInterval(nextImage, 3000); // Change image every 3 seconds
-      return () => clearInterval(timer);
-    }, [currentImageIndex]);
-  
-    return (
-        <div className="carousel banner">
-          <button className="prev" onClick={prevImage}>&lt;</button>
-          <img className='bannerimg'src={images[currentImageIndex].file} alt={`Image ${currentImageIndex}`} />
-          <button className="next" onClick={nextImage}>&gt;</button>
-        </div>
-      );
-    };
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const timer = setInterval(nextImage, 3000); // Change image every 3 seconds
+    return () => clearInterval(timer);
+  }, [currentImageIndex]);
+
+  return (
+      <div className="carousel banner">
+        <button className="prev" onClick={prevImage}>&lt;</button>
+        <img className='bannerimg'src={images[currentImageIndex].file} alt={`Image ${currentImageIndex}`} />
+        <button className="next" onClick={nextImage}>&gt;</button>
+      </div>
+    );
+  };
 
 export default Banner;
