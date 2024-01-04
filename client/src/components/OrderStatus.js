95f0278a6ee104e86b9637942527b96221f99255
Louis Klein <louisklein71@gmail.com>
2024-01-04 00:43:59 -0800

    order checker done

--- client/src/components/OrderStatus.js
+++ client/src/components/OrderStatus.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {useNavigate} from 'react-router-dom';
+import { SessionContext } from '../App'; 
 
 const OrderStatus = () => {
-  // if order number doesn't exist, 404 => alert(Could not find order number, please double check)
-  // if exist, nav => order/order.id
+  const { sessionId, cart, setCart, API_BASE_URL } = useContext(SessionContext);
+  const navigate = useNavigate();
+  const handleFindOrder = (e) => {
+    e.preventDefault();
+    const orderSearch = e.target.children[2].value;
+    findOrder(orderSearch);
+  }
+
+  const findOrder = async (orderId) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/orders/${orderId}`);
+      if (response.ok) {
+        const orderItem = await response.json();
+        orderItem.cart = JSON.parse(orderItem.cart);
+        console.log(orderItem)
+        navigate(`/order/${orderId}`, { state: { orderItem } });
+      } else {
+        console.error('Failed to find order:', response.status);
+      }
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
   return (
     <div>
-      <form>
-        <h4>Enter your order number:</h4>
+      <form onSubmit={handleFindOrder}>
+        <h1 style={{marginTop:'60px', marginBottom:'100px'}}>Enter your order number:</h1>
         <br/>
-        <input type="text" placeholder="Order Number"></input>
+        <input type="text" placeholder="Order Number" style={{marginBottom:'30px'}}></input>
         <br/>
-        <button type="submit" className="logbutton">Check Order Status</button>
+        <button type="submit" className="logbutton" style={{marginBottom:'300px'}}>Check Order Status</button>
       </form>
     </div>
   )
