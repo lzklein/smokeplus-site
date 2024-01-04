0139076d48857de7705779a812bf54c8fd9fdd45
Louis Klein <louisklein71@gmail.com>
2024-01-04 00:43:49 -0800

    Create OrderCheck.js

---
+++ client/src/components/OrderCheck.js
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import CartCard from './CartCard'
+import { SessionContext } from '../App'; 
+
+const OrderCheck = () => {
+    const { sessionId, cart, setCart, API_BASE_URL } = useContext(SessionContext);
+    const location = useLocation();
+    const order = location.state?.orderItem;  
+    console.log(order)
+
+    const renderCard = () => {
+        return order.cart.map((item) => (
+          <CartCard key={item.id} sessionId = {sessionId} setCart = {setCart} item={item} url={API_BASE_URL} order={true}/>
+        ));
+      }
+
+  return (
+    <div>
+        <h1>Your Order</h1>
+        <h2>Order Number {order.id}</h2>
+        {renderCard()}
+    </div>
+  )
+}
+
+export default OrderCheck
