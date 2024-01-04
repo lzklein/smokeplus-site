0845dd9a82ddfb2125bfe1b7e102e4750194e160
Louis Klein <louisklein71@gmail.com>
2024-01-04 00:46:06 -0800

    Update Inbox.js

--- client/src/components/employees/Inbox.js
+++ client/src/components/employees/Inbox.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {useNavigate} from 'react-router-dom';
 
 const Inbox = () => {
   const navigate= useNavigate();
-  // every midnight delete all inbox items/all "confirmed" items or something
+  const [orders, setOrders] = useState([]);
+
   return (
     <div>Inbox
 
