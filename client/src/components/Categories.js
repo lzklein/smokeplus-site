ee8508897c8c3b7058c5b683d18a0951db0f77a9
Louis Klein <louisklein71@gmail.com>
2024-01-05 15:23:07 -0800

    adding categories

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import CategoryCards from './CategoryCards';
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <div>
+      <h1>Shop By Category</h1>
+      <div className='section'>
+        <h3>Cigarettes</h3>
+        <Link className="see-all-link" to={{pathname:'/category/more', state:{category:'Cigarettes'}}}>See More &raquo;</Link>
+      </div>
+      <CategoryCards category='Cigarettes'/>
+      <div className='section'>
+        <h3>Vapes</h3>
+        <Link className="see-all-link" to={{pathname:'/category/more', state:{category:'Vapes'}}}>See More &raquo;</Link>
+      </div>   
+      <CategoryCards category='Vapes'/>
+      <div className='section'>
+        <h3>Tobacco</h3>
+        <Link className="see-all-link" to={{pathname:'/category/more', state:{category:'Tobacco'}}}>See More &raquo;</Link>
+      </div>    
+      <CategoryCards category='Tobacco'/>
+      <div className='section'>
+        <h3>General Merchandise</h3>
+        <Link className="see-all-link" to={{pathname:'/category/more', state:{category:'General Merchandise'}}}>See More &raquo;</Link>
+      </div>
+      <CategoryCards category='General Merchandise'/>
+    </div>
   )
 }
 
