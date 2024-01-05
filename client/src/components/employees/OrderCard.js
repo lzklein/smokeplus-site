4c88a4f81dde6e835df84b9f5141adab861cdcb2
Louis Klein <louisklein71@gmail.com>
2024-01-04 19:19:16 -0800

    Create OrderCard.js

---
+++ client/src/components/employees/OrderCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderCard = ({order}) => {
+    // console.log(order)
+  return (
+    <div>
+        <h3>Order number {order.id}</h3>
+    </div>
+  )
+}
+
+export default OrderCard
