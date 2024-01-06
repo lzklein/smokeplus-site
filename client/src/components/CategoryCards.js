360ac22028f8389f0de4e89e0ec89680c8041b34
Louis Klein <louisklein71@gmail.com>
2024-01-05 16:28:08 -0800

    category cards done

--- client/src/components/CategoryCards.js
+++ client/src/components/CategoryCards.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react';
+import { SessionContext } from '../App';
+import ProductCard from './ProductCard';
 
-const CategoryCards = () => {
+const CategoryCards = ({ category }) => {
+  const { sessionId, cart, setCart, API_BASE_URL } = useContext(SessionContext);
+  const [products, setProducts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    fetchCategoryProducts();
+  }, []);
+
+  const fetchCategoryProducts = async () => {
+    try {
+      const productResponse = await fetch(`${API_BASE_URL}/api/products/related-products/${category}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (productResponse.ok) {
+        const productData = await productResponse.json();
+        setProducts(productData);
+        setLoaded(true);
+      }
+    } catch (error) {
+      console.error('Error fetching related products:', error);
+    }
+  };
+
+  const renderCategoryProducts = () => {
+    return products.map((product) => {
+        return (<div key={product.id} className='productcard'>
+        <ProductCard product = {product} related={true}/>
+      </div>)
+    });
+  };
+
+  if (!loaded) {
+    return <div>Loading</div>;
+  }
+
+  if (products.length === 0) {
+    return <div>Oops No Products. Site Not Updated</div>;
+  }
+
   return (
-    <div>CategoryCards</div>
-  )
-}
+    <div className='related-products'>{renderCategoryProducts()}</div>
+  );
+};
 
-export default CategoryCards
+export default CategoryCards;
