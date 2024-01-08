62e921192e24c688d86b2be4a57795dbd2fd2368
Louis Klein <louisklein71@gmail.com>
2024-01-07 19:19:07 -0800

    Update EmployeeHome.js

--- client/src/components/employees/EmployeeHome.js
+++ client/src/components/employees/EmployeeHome.js
@@ -6,12 +6,12 @@ const EmployeeHome = () => {
   return (
     <div><h1>Smoke Plus Employees Portal</h1>
       <nav>
-        <ul>
-          <li><Link to="/inbox">Inbox</Link></li>
-          <li><Link to='/upload'>Upload Excel</Link></li>
-          <li><Link to="/banner/edit">Edit Banner</Link></li>
-          <li><Link to='/inventory/edit'>Edit Inventory</Link></li> 
-          <li><Link to='/deals/edit'>Edit Deals</Link></li>
+        <ul className='employee-menu'>
+          <li className='employee-link'><Link to="/inbox">Inbox</Link></li>
+          <li className='employee-link'><Link to='/upload'>Upload Excel</Link></li>
+          <li className='employee-link'><Link to="/banner/edit">Edit Banner</Link></li>
+          <li className='employee-link'><Link to='/inventory/edit'>Edit Inventory</Link></li> 
+          <li className='employee-link'><Link to='/deals/edit'>Edit Deals</Link></li>
         </ul>
       </nav>
     </div>
