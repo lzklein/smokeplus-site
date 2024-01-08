95cb7c8885c6077f77d53e7bb606404ccc108079
Louis Klein <louisklein71@gmail.com>
2024-01-08 15:44:41 -0800

    header text change

--- client/src/components/NavRight.js
+++ client/src/components/NavRight.js
@@ -5,7 +5,7 @@ const NavRight = ({cart}) => {
   return (
     <div style={{marginRight:'20px'}}>
         <Link to="/all" className="headnav" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>All Products</Link>
-        <Link to="/categories" className="headnav" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Shop by Category</Link>
+        <Link to="/categories" className="headnav" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Top Categories</Link>
         <Link to="/order" className="headnav" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Order Status</Link>
         <Link to="/cart" className="headnav" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Cart ({cart.length})</Link>
     </div>
