30fc91b8a9f5b297e189475cf9a60095e8bae49f
Louis Klein <louisklein71@gmail.com>
2024-01-09 22:21:15 -0800

    footer visual update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,45 +5,33 @@ import Map from './Map';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-left">
-        <div>
-          <ul>
-            <li><h4>About Us</h4></li>
-            <ul className="foot-sub">
-              <li><a href="/about">About</a></li>
-              <li><a href="/terms">Terms and Policies</a></li>
-              <li><a href="/careers">Careers</a></li>
-            </ul>
-            <li><h4>Stay Connected</h4></li>
-            <ul className="foot-sub">
-              {/* TODO link new tab socials */}
-              <li><a href="/twit">Twitter</a></li>
-              <li><a href="https://www.instagram.com/smoke_plus_renton/?hl=en" target="_blank">Instagram</a></li>
-              <li><a href="/fb">Facebook</a></li>
-            </ul>
-          </ul>
-        </div>
-      </div>
-      <div className="footer-center">
+
+      <div className="footer-center" style={{marginLeft:'300px', marginTop:'20px'}}>
         <ul>
           <li><h4>Contact Us</h4></li>
           <ul className="foot-sub">
-            <li><p>Phone #: (425)264-5749</p></li>
+            <li><p>Phone (425)264-5749</p></li>
             {/* link to email sender thing maybe */}
-            <li><a href="/terms">Email: </a></li>
           </ul>
-          <li><h4>Employee Resources</h4></li>
+          <li><h4 style={{marginTop:'50px'}}>Follow us on Instagram!</h4></li>
+            <ul className="foot-sub">
+              {/* TODO link new tab socials */}
+              {/* <li><a href="/twit">Twitter</a></li> */}
+              <li><a href="https://www.instagram.com/smoke_plus_renton/?hl=en" target="_blank">Instagram</a></li>
+              {/* <li><a href="/fb">Facebook</a></li> */}
+            </ul>
+          <li><h4 style={{marginTop:'50px'}}>Employee Resources</h4></li>
           <ul className="foot-sub">
             <li><a href="/employee-login">Login</a></li>
           </ul>
         </ul>
       </div>
-      <div className="footer-right">
+      <div className="footer-right" style={{marginRight:'10vw'}}>
         {/* Right column content (map) */}
         <div id="map-container">
           <Map />
         </div>
-        <p>
+        <p className='map-address' style={{marginLeft:'190px'}}>
           SMOKE PLUS, 1222 Bronson Way N UNIT 110, Renton, WA 98057
         </p>
       </div>
