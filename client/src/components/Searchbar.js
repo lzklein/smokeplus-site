cbb978bb255067ef6fe0dde97089dfeb23476a19
Louis Klein <louisklein71@gmail.com>
2024-01-18 13:08:52 -0800

    only sticky on mobile

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { SessionContext } from '../App';
 
 const Searchbar = () => {
+  const { addToCart, isMobile } = useContext(SessionContext);
   const [searchText, setSearchText] = useState('');
   const navigate = useNavigate();
 
@@ -18,6 +20,24 @@ const Searchbar = () => {
       handleSearch();
     }
   };
+  
+  const handleScroll = () => {
+    if (isMobile) {
+      const searchContainer = document.querySelector('.search-container');
+  
+      if (searchContainer) {
+        const scrollPosition = window.scrollY;
+  
+        if (scrollPosition >= 60) {
+          searchContainer.classList.add('sticky');
+        } else {
+          searchContainer.classList.remove('sticky');
+        }
+      }
+    }
+  };
+  
+  window.addEventListener('scroll', handleScroll);
 
   return (
     <div className="search-container">
