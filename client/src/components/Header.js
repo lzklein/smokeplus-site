c193b61f4dde7892562e79ad08d22d3be3a04eb7
Louis Klein <louisklein71@gmail.com>
2024-01-18 13:11:12 -0800

    hamburger in mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import NavLeft from './NavLeft';
 import NavRight from './NavRight';
 import { SessionContext } from '../App'; 
 
+
 const Header = () => {
   const { sessionId, cart, isMobile } = useContext(SessionContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleBurger = () => {
+    setIsOpen(!isOpen);
+  };
 
   if(isMobile){
     return(
       <header className="header" style={{ userSelect: 'none' }}>
       <div className="left-nav">
-        <NavLeft />
+        <div className={`hamburger-container ${isOpen ? 'open' : ''}`} onClick={handleBurger}>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+        </div>
+      </div>
+      <div className="right-nav" style={{ userSelect: 'none' }}>
+        <NavRight cart={cart}/>
       </div>
     </header>
     )
