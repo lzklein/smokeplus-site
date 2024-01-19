b29aaf3add6b9e68071d5cc0cd885d75f0d8136f
Louis Klein <louisklein71@gmail.com>
2024-01-19 02:34:12 -0800

    footer fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,19 +13,19 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="footer-center">
+      <div className="footer-center" style={{marginLeft:'20vw', marginTop:'25px'}}>
         <ul>
           <li>
             <h4>Contact Us</h4>
           </li>
           <li>
-            <p>Phone (425) 264-5749</p>
+            <p style={{fontSize:'12px', marginBottom:'10px'}}> Phone (425) 264-5749</p>
           </li>
           <li>
             <h4>Follow us on Instagram!</h4>
           </li>
           <li>
-            <a href="https://www.instagram.com/smoke_plus_renton/?hl=en" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/smoke_plus_renton/?hl=en" target="_blank" rel="noopener noreferrer" style={{fontSize:'12px'}}>
               Instagram
             </a>
           </li>
@@ -33,11 +33,11 @@ const Footer = () => {
             <h4>Employee Resources</h4>
           </li>
           <li>
-            <a href="/employee-login">Login</a>
+            <a href="/employee-login" style={{fontSize:'12px'}}>Login</a>
           </li>
         </ul>
       </div>
-      <div className="footer-right">
+      <div className="footer-right" style={{marginRight:'5vw'}}>
         <div id="map-container">
           <a
             href="https://www.google.com/maps/dir//SMOKE+PLUS,+1222+Bronson+Way+N+UNIT+110,+Renton,+WA+98057/@47.4834672,-122.2039636,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x54906801b0adccd3:0x1a5833d1f30e5aa8!2m2!1d-122.201274!2d47.4834422!3e0?entry=ttu"
