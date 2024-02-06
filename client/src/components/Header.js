c5a34101c6c6e9f89352117a15aabe9eac3de601
Louis Klein <louisklein71@gmail.com>
2024-02-05 21:16:47 -0800

    improved hamburger animation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,14 +6,16 @@ import { SessionContext } from '../App';
 
 
 const Header = () => {
-  const { sessionId, cart, isMobile } = useContext(SessionContext);
+  const { cart, isMobile } = useContext(SessionContext);
   const [isOpen, setIsOpen] = useState(false);
   const [hidden, setHidden] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
 
   const handleBurger = () => {
     setIsOpen(!isOpen);
     setHidden(!hidden);
+    setIsMenuOpen(!isMenuOpen);
   };
 
   useEffect(() => {
@@ -21,44 +23,30 @@ const Header = () => {
     setHidden(true);
   }, [location.pathname]);
 
-  if(isMobile){
-    return(
-      <header className="header" style={{ userSelect: 'none' }}>
-      <div className="left-nav">
-        <div className={`hamburger-container ${isOpen ? 'open' : ''}`} onClick={handleBurger}>
-          <div className="hamburger-line"></div>
-          <div className="hamburger-line"></div>
-          <div className="hamburger-line"></div>
-        </div>
-        <Link to="/" style={{marginLeft:'50px', fontSize:'30px', fontWeight:'500'}}>SMOKE PLUS</Link>
-      </div>
-      {!hidden && (
-          <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
-            <Link to="/all" className="hamburger-link" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>All Products</Link>
-            <Link to="/categories" className="hamburger-link" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Top Categories</Link>
-            <Link to="/order" className="hamburger-link" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Order Status</Link>
-          </div>
-        )}
-        
-      <div className="right-nav" style={{ userSelect: 'none' }}>
-        <NavRight cart={cart}/>
-      </div>
-    </header>
-    )
-  }
-
   return (
     <header className="header" style={{ userSelect: 'none' }}>
       <div className="left-nav">
-        <div className={`hamburger-container ${isOpen ? 'open' : ''}`} onClick={handleBurger}>
-          <div className="hamburger-line"></div>
-          <div className="hamburger-line"></div>
-          <div className="hamburger-line"></div>
-        </div>
+        <nav role="navigation">
+          <div id="menuToggle" onClick={handleBurger}>
+            <input type="checkbox" />
+
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <ul id="menu" className={isMenuOpen ? 'isMenuOpen' : ''}>
+              <a href="#"><li>Home</li></a>
+              <a href="#"><li>About</li></a>
+              <a href="#"><li>Info</li></a>
+              <a href="#"><li>Contact</li></a>
+              <a href="https://erikterwan.com/" target="_blank" rel="noopener noreferrer"><li>Show me more</li></a>
+            </ul>
+          </div>
+        </nav>
         <NavLeft />
       </div>
 
-      <div className="right-nav" style={{ userSelect: 'none' }}>
+      <div className={`right-nav ${isMenuOpen ? 'isMenuOpen' : ''}`} style={{ userSelect: 'none' }}>
         <NavRight cart={cart}/>
       </div>
     </header>
