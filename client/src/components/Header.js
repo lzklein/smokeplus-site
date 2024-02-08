d02b6b5132fbeed903f28846422a2b3b14b9ad82
Louis Klein <louisklein71@gmail.com>
2024-02-08 05:26:46 -0800

    revert for mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -22,6 +22,32 @@ const Header = () => {
     setIsOpen(false);
     setHidden(true);
   }, [location.pathname]);
+  
+  if(isMobile){
+    return(
+      <header className="header" style={{ userSelect: 'none' }}>
+      <div className="left-nav">
+        <div className={`hamburger-container ${isOpen ? 'open' : ''}`} onClick={handleBurger}>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+        </div>
+        <Link to="/" style={{marginLeft:'50px', fontSize:'30px', fontWeight:'500'}}>SMOKE PLUS</Link>
+      </div>
+      {!hidden && (
+          <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+            <Link to="/all" className="hamburger-link" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>All Products</Link>
+            <Link to="/categories" className="hamburger-link" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Top Categories</Link>
+            <Link to="/order" className="hamburger-link" style={{marginRight:'100px', fontSize:'20px', fontWeight:'500'}}>Order Status</Link>
+          </div>
+        )}
+        
+      <div className="right-nav" style={{ userSelect: 'none' }}>
+        <NavRight cart={cart}/>
+      </div>
+    </header>
+    )
+  }
 
   return (
     <header className="header" style={{ userSelect: 'none' }}>
