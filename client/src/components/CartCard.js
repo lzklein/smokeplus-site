ba35f48216e4ef8a5fb13d2d0f31e5804be33aa0
Louis Klein <louisklein71@gmail.com>
2024-03-05 21:48:33 -0800

    Update CartCard.js

--- client/src/components/CartCard.js
+++ client/src/components/CartCard.js
@@ -139,7 +139,7 @@ const CartCard = ({ sessionId, setTotal, setCart, item, url, order }) => {
             </div>
           </div>
           {!isMobile && <br />}
-          {!isMobile && (product.deals ? <p>${getPrice(product.price)}</p> : <p>${(product.price * item.quantity).toFixed(2)}</p>)}
+          {!isMobile && (product.deals ? <p style={{paddingLeft:'10px'}}>${getPrice(product.price)}</p> : <p  style={{paddingLeft:'10px'}}>${(product.price * item.quantity).toFixed(2)}</p>)}
           <button className="backbutton" onClick={handleDelete} style={{ marginRight: '300px', marginLeft: '30px', marginTop: '16px' }}>{isMobile?'x':'Remove from Cart'}</button>
         </div>
       ) : (
