0ec0dc481035489013307b367ca3ac4cd24ee76b
Louis Klein <louisklein71@gmail.com>
2024-03-08 15:06:30 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -199,16 +199,19 @@ const App = () => {
 
   if (!!loading) {
     return (
-      <div class="whirlpool">
-        <div class="ring ring1"></div>
-        <div class="ring ring2"></div>
-        <div class="ring ring3"></div>
-        <div class="ring ring4"></div>
-        <div class="ring ring5"></div>
-        <div class="ring ring6"></div>
-        <div class="ring ring7"></div>
-        <div class="ring ring8"></div>
-        <div class="ring ring9"></div>
+      <div>
+        <div class="whirlpool" style={{marginTop:'200px'}}>
+          <div class="ring ring1"></div>
+          <div class="ring ring2"></div>
+          <div class="ring ring3"></div>
+          <div class="ring ring4"></div>
+          <div class="ring ring5"></div>
+          <div class="ring ring6"></div>
+          <div class="ring ring7"></div>
+          <div class="ring ring8"></div>
+          <div class="ring ring9"></div>
+        </div>
+        <h1>Loading...</h1>
       </div>
     );
   }
